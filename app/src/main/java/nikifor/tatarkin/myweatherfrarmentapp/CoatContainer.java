fe81359c493eceb0757f3b81622b590ee6e2a6fe
NikiforT <nikifor.tatarkin@yandex.ru>
2020-03-29 19:07:20 +0300

    Lesson5

---
+++ app/src/main/java/nikifor/tatarkin/myweatherfrarmentapp/CoatContainer.java
@@ -0,0 +1,10 @@
+package nikifor.tatarkin.myweatherfrarmentapp;
+
+import java.io.Serializable;
+
+public class CoatContainer implements Serializable {
+    public int position = 0;
+    public String cityName = "";
+    public boolean visibilitySpeed = true;
+    public boolean visibilityPressure;
+}
--- app/src/main/java/nikifor/tatarkin/myweatherfrarmentapp/fragment/InfoFragment.java
+++ app/src/main/java/nikifor/tatarkin/myweatherfrarmentapp/fragment/InfoFragment.java
@@ -1,65 +1,92 @@
-package nikifor.tatarkin.myweatherfrarmentapp;
+package nikifor.tatarkin.myweatherfrarmentapp.fragment;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.squareup.otto.Subscribe;
+
+import java.util.Objects;
 
 import nikifor.tatarkin.myweatherfrarmentapp.ClickedEvent.FragmentBtnClickedPressureEvent;
 import nikifor.tatarkin.myweatherfrarmentapp.ClickedEvent.FragmentBtnClickedSpeedEvent;
+import nikifor.tatarkin.myweatherfrarmentapp.CoatContainer;
+import nikifor.tatarkin.myweatherfrarmentapp.Constants;
+import nikifor.tatarkin.myweatherfrarmentapp.R;
+import nikifor.tatarkin.myweatherfrarmentapp.TemperatureAdapter;
+
+import static androidx.recyclerview.widget.LinearLayoutManager.HORIZONTAL;
 
 public class InfoFragment extends Fragment implements Constants {
     private static final String INDEX_CITY = "index";
-    private static final String KEY_TEXT_SPEED = "key speed";
-    private static final String KEY_TEXT_PRESSURE = "key pressure";
 
     private Button buttonAboutCityBrowser;
     private TextView textSpeedWind;
     private TextView textPressure;
     private TextView textNameCity;
 
-    boolean visibleSpeedBoolean;
-    boolean visiblePressureBoolean;
+    private RecyclerView recyclerView;
+
+    //Фейковые данные для температуры на 3 дня.
+    private String[] listTemperature = new String[]{"+ 24'", "+ 23'", "+ 19'"};
 
     // Фабричный метод создания фрагмента
-    public static InfoFragment create(int index, boolean visibleSpeed, boolean visiblePressure) {
+    public static InfoFragment create(CoatContainer container) {
         InfoFragment infoFragment = new InfoFragment();    // создание
 
         // Передача параметров
         Bundle args = new Bundle();
-        args.putInt(INDEX_CITY, index);
-        args.putBoolean(SPEED_VISIBLE, visibleSpeed);
-        args.putBoolean(PRESSURE_VISIBLE, visiblePressure);
+        args.putSerializable(INDEX_CITY, container);
         infoFragment.setArguments(args);
         return infoFragment;
     }
 
     // Получить индекс из списка (фактически из параметра)
     public int getIndex() {
-        int index = getArguments().getInt(INDEX_CITY, 0);
-        return index;
+        CoatContainer coatContainer = (CoatContainer) (Objects.requireNonNull(getArguments())
+                .getSerializable(INDEX_CITY));
+        try {
+            return coatContainer.position;
+        } catch (Exception e) {
+            return 0;
+        }
     }
 
+    //Получить информацию о том, нужно ли показывать скорость ветра
     public boolean getVisibSpeed(){
-        visibleSpeedBoolean = getArguments().getBoolean(SPEED_VISIBLE);
-        return visibleSpeedBoolean;
+        CoatContainer coatContainer = (CoatContainer) (Objects.requireNonNull(getArguments())
+                .getSerializable(INDEX_CITY));
+        try {
+            return coatContainer.visibilitySpeed;
+        } catch (Exception e) {
+            return true;
+        }
     }
 
+    //Получить информацию о том, нужно ли показывать давление
     public boolean getVisibPressure(){
-        visiblePressureBoolean = getArguments().getBoolean(PRESSURE_VISIBLE);
-        return visiblePressureBoolean;
+        CoatContainer coatContainer = (CoatContainer) (Objects.requireNonNull(getArguments())
+                .getSerializable(INDEX_CITY));
+        try {
+            return coatContainer.visibilityPressure;
+        } catch (Exception e) {
+            return true;
+        }
     }
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Определить какой город надо показать, и показать его
@@ -71,14 +98,32 @@ public class InfoFragment extends Fragment implements Constants {
         super.onViewCreated(view, savedInstanceState);
 
         initViews(view);
+        initRecyclerView(view);
         setTextNameCity();
 
         //Показать-скрыть скоростьи давление при открытии фрагмента на новой активити.
         setVisibilitySpeed();
         setVisibilityPressure();
 
-
         openBrowser();
+    }
+
+    // Инциализация RecyclerView для показа температуры на3 дня.  + Декоратор
+    private void initRecyclerView(@NonNull View view) {
+        recyclerView = view.findViewById(R.id.recycler_view_temp);
+
+        //Декоратор - начало.
+        DividerItemDecoration itemDecoration = new DividerItemDecoration(getContext(), HORIZONTAL);
+        itemDecoration.setDrawable(getContext().getDrawable(R.drawable.separator_temperature));
+        recyclerView.addItemDecoration(itemDecoration);
+        //Декоратор - конец.
+
+        TemperatureAdapter adapter = new TemperatureAdapter(listTemperature);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), HORIZONTAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+
+
     }
 
     //Инициализация полей
@@ -132,7 +177,7 @@ public class InfoFragment extends Fragment implements Constants {
     }
     //Показать/Скрыть скорость ветка и давление - конец.
 
-
+    //Переопределенные методы для использования EventBus.
     @Override
     public void onStart() {
         super.onStart();
